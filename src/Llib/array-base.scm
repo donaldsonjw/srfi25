ea7f8d2c4acf443866a269a41afe6db89c970ba5
Joseph Donaldson <donaldsonjw@yahoo.com>
2020-04-05 14:29:05 -0700

    Update Makefile for newer versions of gnu make
    Also added files mistakenly omitted from git

---
+++ src/Llib/array-base.scm
@@ -0,0 +1,14 @@
+(module srfi25/array-base
+   (export      
+      (abstract-class %array-base
+         startv::vector
+         endv::vector
+         scratch::vector)
+      (final-class %array::%array-base
+         coeff::vector
+         store)
+      (final-class %shared-array::%array-base
+         constants::vector
+         coeffs::vector
+         sscratch::vector
+         backing::%array-base)))
